1953c2c968424d95b34c4942dfc8903423853203
Ubiquity89 <ubiquity89@gmail.com>
2026-06-23 02:27:30 +0530

    added route content to main page

--- product-browser/src/server.js
+++ product-browser/src/server.js
@@ -14,6 +14,12 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Product Browser API is running"
+  });
+});
+
 app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
